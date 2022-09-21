57e07ddf9dd2c9d6408d67899c1ed12bbfdfd69c
glensorbo <glensorbo@gmail.com>
2022-09-21 23:19:51 +0200

    feat: :sparkles: adds fluentvalidator

--- WebPortal.Application/DependencyInjection.cs
+++ WebPortal.Application/DependencyInjection.cs
@@ -1,5 +1,9 @@
+using System.Reflection;
+using ErrorOr;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using WebPortal.Application.Common.Interfaces.Behaviors;
 
 namespace WebPortal.Application;
 
@@ -8,6 +12,10 @@ public static class DependencyInjection
   public static IServiceCollection AddApplication(this IServiceCollection services)
   {
     services.AddMediatR(typeof(DependencyInjection).Assembly);
+
+    services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+    services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
     return services;
   }
